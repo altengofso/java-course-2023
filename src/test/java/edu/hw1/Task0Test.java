5d345a720a74de97dd112f9de32050c2f0c2368e
Altengof Sergey <altengofso@gmail.com>
2023-10-06 01:25:36 +0300

    add task 0 test

---
+++ src/test/java/edu/hw1/Task0Test.java
@@ -0,0 +1,23 @@
+package edu.hw1;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class Task0Test {
+    @Test
+    @DisplayName("Log Message Hello World!")
+    void testLogHelloWorld() {
+        ByteArrayOutputStream logOutput = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(logOutput));
+
+        Task0.logHelloWorld();
+
+        String logMessage = logOutput.toString();
+        assertThat(logMessage).contains("Привет, мир!");
+    }
+}
