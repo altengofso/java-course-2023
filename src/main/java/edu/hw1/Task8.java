5e341928d02d70d6705ce477e0614edbff2400c7
Altengof Sergey <altengofso@gmail.com>
2023-10-06 00:03:26 +0300

    solving task 8

---
+++ src/main/java/edu/hw1/Task8.java
@@ -0,0 +1,27 @@
+package edu.hw1;
+
+public final class Task8 {
+    private Task8() {
+    }
+
+    public static boolean knightBoardCapture(int[][] board) {
+        int[] di = { -2, -1, 1, 2, 2, 1, -1, -2 };
+        int[] dj = { 1, 2, 2, 1, -1, -2, -2, -1 };
+        for (int i = 0; i < 8; ++i) {
+            for (int j = 0; j < 8; ++j) {
+                if (board[i][j] == 1) {
+                    for (int k = 0; k < 8; ++k) {
+                        int ti = i + di[k];
+                        int tj = j + dj[k];
+                        if (ti >= 0 && ti < 8 && tj >= 0 && tj < 8) {
+                            if (board[ti][tj] == 1) {
+                                return false;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return true;
+    }
+}
