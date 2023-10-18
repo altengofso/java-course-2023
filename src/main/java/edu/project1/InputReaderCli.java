1f075fd523b69a49d060f32c6ac008c72b3d878d
altengofso <altengofso@gmail.com>
2023-10-18 14:56:52 +0300

    implement ConsoleHangman Game

---
+++ src/main/java/edu/project1/InputReaderCli.java
@@ -0,0 +1,16 @@
+package edu.project1;
+
+import java.util.Scanner;
+
+public class InputReaderCli implements InputReader {
+    private final Scanner scanner;
+
+    public InputReaderCli(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    @Override
+    public String read() {
+        return scanner.nextLine();
+    }
+}
---
+++ src/main/java/edu/project1/SessionLastState.java
@@ -0,0 +1,11 @@
+package edu.project1;
+
+public enum SessionLastState {
+    NONE,
+    SUCCESS,
+    FAIL,
+    DEFEAT,
+    WON,
+    PLAYAGAIN,
+    GOODBYE
+}
