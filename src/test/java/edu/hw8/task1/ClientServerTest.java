31b5e8c4778a6d42941ef068823ee67299e67491
altengofso <altengofso@gmail.com>
2023-12-03 22:08:50 +0300

    fix port number

--- src/test/java/edu/hw8/task1/ClientServerTest.java
+++ src/test/java/edu/hw8/task1/ClientServerTest.java
@@ -11,7 +11,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class ClientServerTest {
     private final String endpoint = "localhost";
-    private final int port = 12345;
+    private final int port = 8080;
 
     static Stream<Arguments> ClientServerArguments() {
         return Stream.of(
