b4a854f1783c8a83c0e378d2a641b6bcf10a9158
altengofso <altengofso@gmail.com>
2023-12-06 23:37:58 +0300

    change queue type

--- src/main/java/edu/hw9/task3/MultiThreadMazeSolver.java
+++ src/main/java/edu/hw9/task3/MultiThreadMazeSolver.java
@@ -7,12 +7,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Queue;
 import java.util.Set;
-import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.atomic.AtomicBoolean;
 import lombok.SneakyThrows;
 
